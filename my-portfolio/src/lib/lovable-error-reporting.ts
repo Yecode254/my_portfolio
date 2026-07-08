d32909053ca843d9113510c8a6d45bbdf735ea3c
Yego <eddieyego273@gmail.com>
2026-07-08 12:21:15 +0300

    changed error function

--- my-portfolio/src/lib/lovable-error-reporting.ts
+++
@@ -1,6 +0,0 @@
-export function reportErrorToLovable(error: unknown) {
-  // placeholder for lovable error reporting integration
-  // noop for now
-  // eslint-disable-next-line no-console
-  console.warn('Reporting error to lovable (stub)', error);
-}
